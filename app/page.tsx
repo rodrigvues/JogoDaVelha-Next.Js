598485f676a80cdb966127a168c306b13a055c17
Vitor Rodrigues <129552443+rodrigvues@users.noreply.github.com>
2023-11-30 16:38:02 -0300

    adicionando tbody

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,24 @@ export default function Home() {
     <div>
       <h1>Jogo da Velha</h1>
       <h2>jogue com moderação</h2>
-      <title>
-        Jogo da Velha 
-      </title>
       <table>
-        <tr>
-          <td>X</td>
-          <td>X</td>
-          <td>X</td>
-        </tr>
-        <tr>
-          <td>X</td>
-          <td>X</td>
-          <td>X</td>
-        </tr>
-        <tr>
-          <td>X</td>
-          <td>X</td>
-          <td>X</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>X</td>
+            <td>X</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>X</td>
+            <td>X</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>X</td>
+            <td>X</td>
+            <td>X</td>
+          </tr>
+        </tbody>
       </table>
       <button>
         <a href="/">Reiniciar</a>
