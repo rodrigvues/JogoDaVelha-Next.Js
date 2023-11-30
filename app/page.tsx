354e5c6f7493f6bcc0b29319f9a2ec1c43f3ec6f
Vitor Rodrigues <129552443+rodrigvues@users.noreply.github.com>
2023-11-30 14:37:19 -0300

    arrumando html e css 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,29 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <main>
-    </main>
+    <div>
+      <h1>Jogo da Velha</h1>
+      <title>
+        Jogo da Velha 
+      </title>
+      <table>
+        <tr>
+          <td>X</td>
+          <td>X</td>
+          <td>X</td>
+        </tr>
+        <tr>
+          <td>X</td>
+          <td>X</td>
+          <td>X</td>
+        </tr>
+        <tr>
+          <td>X</td>
+          <td>X</td>
+          <td>X</td>
+        </tr>
+      </table>
+    </div>
   )
 }
