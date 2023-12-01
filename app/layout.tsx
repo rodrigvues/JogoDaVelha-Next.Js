c369723d868f92156e259dc866c8ade069249a32
Vitor Rodrigues <129552443+rodrigvues@users.noreply.github.com>
2023-12-01 13:57:30 -0300

    importando nova fonte

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto_Mono } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto_Mono({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +20,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <title>Jogo da Velha</title>
+      <body className={roboto.className}>{children}</body>
     </html>
   )
 }
