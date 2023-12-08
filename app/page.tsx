f28f6adca0a4e77b33aa6a270de4df53ff0b8009
Vitor Rodrigues <129552443+rodrigvues@users.noreply.github.com>
2023-12-08 14:20:30 -0300

    começar jogo por barra de espaço e blink do h2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
+"use client"
 import Link from "next/link";
 import './style.css'
-
+import { useEffect } from 'react';
 
 
 export default function Page() {
-  return(
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.code === 'Space') {
+        // Utilize a função push para redirecionar para a próxima página
+        window.location.href =
+          window.location.pathname === './jogo' ? './jogo' : './jogo';
+      }
+    };
 
-    
+    window.addEventListener('keydown', handleKeyDown);
+
+    // Limpar o evento ao desmontar o componente
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []); // Sem a dependência router
+
+  return(
     <div >
       <div className="titulo">
       <h1>Jogo da Velha</h1>
