fd47b4c77d09a734cf9fffc14baf2c8204ccfa3d
Vitor Rodrigues <129552443+rodrigvues@users.noreply.github.com>
2023-12-08 13:43:47 -0300

    mudado pouca coisa

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 import './style.css'
 
 
+
 export default function Page() {
   return(
-    <div>
+
+    
+    <div >
       <div className="titulo">
       <h1>Jogo da Velha</h1>
       <h2>clique na barra de espaço para jogar</h2>
