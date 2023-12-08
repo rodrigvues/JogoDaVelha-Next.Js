f9e69f3632515a23998de068916cd82aee2dbf2f
Vitor Rodrigues <129552443+rodrigvues@users.noreply.github.com>
2023-12-08 15:19:04 -0300

    arrumando share button

--- app/jogo/page.tsx
+++ app/jogo/page.tsx
@@ -1,9 +1,20 @@
+"use client"
 import { BsArrowCounterclockwise } from "react-icons/bs";
 import { BsFillHouseFill } from "react-icons/bs";
 import { BsShare } from "react-icons/bs";
 
 
 export default function Home() {
+  function copyText() {
+    // Get the text field
+    const copyText = "https://jogo-da-velha-next-peach.vercel.app/";
+  
+    // Select the text field     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText);
+  
+    // Alert the copied text
+    alert("Link copiado para a área de transferência: " + copyText);
+  }
   return (
     <div>
       <h1>Jogo da Velha</h1>
@@ -30,7 +41,7 @@ export default function Home() {
       <div className='button'>
         <a href="/jogo"><BsArrowCounterclockwise /></a>
         <a href="/"><BsFillHouseFill /></a>
-        <a href="/"><BsShare /></a>
+        <a onClick={copyText}><BsShare /></a>
       </div>
     </div>
   )
