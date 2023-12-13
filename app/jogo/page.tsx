0a66663e3f98175089783ac3f8379b0a0358c78d
Vitor Rodrigues <129552443+rodrigvues@users.noreply.github.com>
2023-12-13 14:26:36 -0300

    winner function & overflow home page

--- app/jogo/page.tsx
+++ app/jogo/page.tsx
@@ -25,7 +25,74 @@ export default function Home() {
     if(table[index]) return 
     table[index] = player
     setPlayer(player == "X" ? "O" : "X")
+    checkWinner()
   }
+
+  function checkWinner(){
+    if(table[0] == table[1] && table[1] == table[2] && table[0] != ""){
+      if(player == "X"){
+      alert("O X ganhou");return}
+        else{
+          return alert("O O ganhou");
+        }
+    }
+    
+    if(table[3] == table[4] && table[4] == table[5] && table[3] != ""){
+      if(player == "X"){
+      alert("O X ganhou");return}
+        else{
+          alert("O O ganhou");return
+        }
+    }
+
+    if(table[6] == table[7] && table[7] == table[8] && table[6] != ""){
+      if(player == "X"){
+      alert("O X ganhou");return}
+        else{
+          alert("O O ganhou");return
+        }
+    }
+
+    if(table[0] == table[3] && table[3] == table[6] && table[0] != ""){
+      if(player == "X"){
+      alert("O X ganhou");return}
+        else{
+          alert("O O ganhou");return
+        }
+    }
+
+    if(table[1] == table[4] && table[4] == table[7] && table[1] != ""){
+      if(player == "X"){
+      alert("O X ganhou");return}
+        else{
+          alert("O O ganhou");return
+        }
+    }
+
+    if(table[2] == table[5] && table[5] == table[8] && table[2] != ""){
+      if(player == "X"){
+      alert("O X ganhou");return}
+        else{
+          alert("O O ganhou");return
+        }
+    }
+
+    if(table[0] == table[4] && table[4] == table[8] && table[0] != ""){
+      if(player == "X"){
+      alert("O X ganhou");return}
+        else{
+          alert("O O ganhou");return
+        }
+    }
+    if(table[2] == table[4] && table[4] == table[6] && table[2] != ""){
+      if(player == "X"){
+      alert("O X ganhou");return}
+        else{
+          alert("O O ganhou");return
+        }
+    }
+  }
+
   return (
     <div>
       <h1>Jogo da Velha</h1>
