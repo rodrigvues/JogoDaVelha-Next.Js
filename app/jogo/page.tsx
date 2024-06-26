563322c845e211f47a87544010724dfe876ad619
Vitor Rodrigues <rodrigvues@gmail.com>
2024-06-26 09:16:32 -0300

    centralizando

--- app/jogo/page.tsx
+++ app/jogo/page.tsx
@@ -94,33 +94,36 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <h1>Jogo da Velha</h1>
-      <h2>jogue com moderação</h2>
-      <table>
-        <tbody>
-          <tr>
-            <td onClick={() => mark(0)}>{table[0]}</td>
-            <td onClick={() => mark(1)}>{table[1]}</td>
-            <td onClick={() => mark(2)}>{table[2]}</td>
-          </tr>
-          <tr>
-            <td onClick={() => mark(3)}>{table[3]}</td>
-            <td onClick={() => mark(4)}>{table[4]}</td>
-            <td onClick={() => mark(5)}>{table[5]}</td>
-          </tr>
-          <tr>
-            <td onClick={() => mark(6)}>{table[6]}</td>
-            <td onClick={() => mark(7)}>{table[7]}</td>
-            <td onClick={() => mark(8)}>{table[8]}</td>
-          </tr>
-        </tbody>
-      </table>
-      <div className='button'>
-        <a href="/jogo"><BsArrowCounterclockwise /></a>
-        <a href="/"><BsFillHouseFill /></a>
-        <a onClick={copyText}><BsShare /></a>
-      </div>
-    </div>
+    <body>
+      <section>
+        <h1 className="titulo" >Jogo da Velha</h1>
+        <h2>jogue com moderação</h2>
+        <table>
+          <tbody>
+            <tr>
+              <td onClick={() => mark(0)}>{table[0]}</td>
+              <td onClick={() => mark(1)}>{table[1]}</td>
+              <td onClick={() => mark(2)}>{table[2]}</td>
+            </tr>
+            <tr>
+              <td onClick={() => mark(3)}>{table[3]}</td>
+              <td onClick={() => mark(4)}>{table[4]}</td>
+              <td onClick={() => mark(5)}>{table[5]}</td>
+            </tr>
+            <tr>
+              <td onClick={() => mark(6)}>{table[6]}</td>
+              <td onClick={() => mark(7)}>{table[7]}</td>
+              <td onClick={() => mark(8)}>{table[8]}</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className='button'>
+          <a href="/jogo"><BsArrowCounterclockwise /></a>
+          <a href="/"><BsFillHouseFill /></a>
+          <a onClick={copyText}><BsShare /></a>
+        </div>
+      </section>
+    </body>
+    
   )
 }
